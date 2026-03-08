ad25382f6fa992f1e94d34794eee9096ebe194f2
Brunonunes06 <bruno.nunes.santos06@escola.pr.gov.br>
2026-03-08 12:08:01 -0300

    Cor atualizado

--- main.js
+++ main.js
@@ -193,7 +193,7 @@ document.getElementById("login-form").addEventListener("submit", (e) => {
   const name = email.split("@")[0];
   handleLogin(
     name,
-    "https://ui-avatars.com/api/?name=" + name + "&background=000&color=fff",
+    "https://ui-avatars.com/api/?name=" + name + "&background=D4A373&color=fff",
     email,
   );
 });
@@ -204,7 +204,7 @@ document.getElementById("register-form").addEventListener("submit", (e) => {
   const email = e.target.querySelector('input[type="email"]').value;
   handleLogin(
     name,
-    "https://ui-avatars.com/api/?name=" + name + "&background=000&color=fff",
+    "https://ui-avatars.com/api/?name=" + name + "&background=D4A373&color=fff",
     email,
   );
 });
@@ -415,17 +415,17 @@ function updateLoyaltyUI() {
   if (dashPointsEl) dashPointsEl.innerText = `${userPoints} Pontos`;
   if (userPtsRank) userPtsRank.innerText = `${userPoints} pts`;
 
-  // Lógica de Níveis (Escala Monocromática Premium)
+  // Lógica de Níveis
   let tier = "Bronze";
-  let color = "#cbd5e1"; // Silver-Gray
+  let color = "#CD7F32";
   if (userPoints > 500) {
-    tier = "Ouro Platinum";
-    color = "#000000"; // Black
+    tier = "Ouro";
+    color = "#FFD700";
     const goldBadge = document.getElementById("badge-gold");
     if (goldBadge) goldBadge.classList.remove("locked");
   } else if (userPoints > 200) {
-    tier = "Prata Elite";
-    color = "#94a3b8"; // Steel Gray
+    tier = "Prata";
+    color = "#C0C0C0";
   }
 
   const tierEl = document.getElementById("user-tier");
